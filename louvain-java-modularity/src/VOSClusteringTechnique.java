d2d168f4fca96d354d2362d25b5094e77b74fccb
Jennifer <jn012013@mymail.pomona.edu>
2015-12-03 14:01:17 -0800

    APSP reimplemented

--- louvain-java-modularity/src/VOSClusteringTechnique.java
+++ louvain-java-modularity/src/VOSClusteringTechnique.java
@@ -581,4 +581,58 @@ public class VOSClusteringTechnique {
 
         clustering.mergeClusters(VOSClusteringTechnique.clustering);
     }
+
+    /***********************************************************************
+     * SILHOUETTE INDEX
+     ***********************************************************************/
+    public double[][] floydWarshall(double[][] graph, int nNodes)
+        {
+            double[][] dist;
+            int i, j, k;
+
+            dist = new double[nNodes][nNodes];
+     
+            /* Initialize the solution matrix same as input graph matrix.
+               Or we can say the initial values of shortest distances
+               are based on shortest paths considering no intermediate
+               vertex. */
+            for (i = 0; i < nNodes; i++) {
+                for (j = 0; j < nNodes; j++) {
+                    if (i == j) {
+                        dist[i][j] = 0;
+                    } else {
+                        dist[i][j] = graph[i][j];
+                    }
+                }
+            }
+     
+            /* Add all vertices one by one to the set of intermediate
+               vertices.
+              ---> Before start of a iteration, we have shortest
+                   distances between all pairs of vertices such that
+                   the shortest distances consider only the vertices in
+                   set {0, 1, 2, .. k-1} as intermediate vertices.
+              ----> After the end of a iteration, vertex no. k is added
+                    to the set of intermediate vertices and the set
+                    becomes {0, 1, 2, .. k} */
+            for (k = 0; k < nNodes; k++)
+            {
+                // Pick all vertices as source one by one
+                for (i = 0; i < nNodes; i++)
+                {
+                    // Pick all vertices as destination for the
+                    // above picked source
+                    for (j = 0; j < nNodes; j++)
+                    {
+                        // If vertex k is on the shortest path from
+                        // i to j, then update the value of dist[i][j]
+                        if (dist[i][k] + dist[k][j] < dist[i][j])
+                            dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+     
+            // Print the shortest distance matrix
+            return dist;
+        }
 }
